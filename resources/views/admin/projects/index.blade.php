bb7c9c10465bdb5db3ba06ddc97e63fe2d886191
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-25 19:25:58 +0200

    Feat: added count in index.blade

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -17,7 +17,7 @@
                 <th scope="col">ID</th>
                 <th scope="col">Titolo</th>
                 <th scope="col">Categoria</th>
-                <th scope="col">Tech</th>
+                <th scope="col">N Tech</th>
                 <th scope="col">Contenuto</th>
                 <th scope="col">Slug</th>
                 <th scope="col">Dettagli</th>
@@ -31,6 +31,9 @@
                     <th scope="row">{{ $curProject->id }}</th>
                     <td>{{ $curProject->title }}</td>
                     <td>{{ $curProject->type?->name }}</td>
+                    <td>
+                       {{ count($curProject->technologies) }}             
+                    </td>
                     <td>{{ $curProject->content }}</td>
                     <td>{{ $curProject->slug }}</td>
                     <td>
